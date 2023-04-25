9553a079a0a949f83a4de78783b8da6205dd3e07
juanesc396 <85693288+juanesc396@users.noreply.github.com>
2023-04-25 11:42:48 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from dash import Dash, html, dcc, Input, Output, ctx
+from dash import Dash, html, dcc, Input, Output
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from sqlalchemy import create_engine
 from datetime import datetime
+import os
 
 
 BS5 = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'
 FONT = 'https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+ICONS = {'src': 'https://kit.fontawesome.com/1e71e0f700.js',
+         'crossorigin': 'anonymous'
+         }
 
-newspapers_dataset = pd.read_excel('./filtered_analized_data.xlsx')
-by_country = pd.read_csv('./by_country.csv')
+RDSIP = os.environ.get('RDSIP')
+RDSPW = os.environ.get('RDSPW')
+mysql_rds = create_engine(
+    f'mysql+pymysql://admin:{RDSPW}@{RDSIP}/newspaper_analytics')
+
+query = f"""SELECT * FROM news 
+            WHERE DATE(scrape_date) BETWEEN 
+                DATE_SUB(NOW(), INTERVAL 10 DAY) 
+                AND NOW()
+"""
+newspapers_dataset = pd.read_sql(sql=query, con=mysql_rds)
+
 
 country_keys = sorted(newspapers_dataset['country_key'].unique().tolist())
 total_news_by_country = newspapers_dataset.groupby(by='country').count()[
@@ -21,10 +36,21 @@ news_genres = ['culture', 'economy', 'entertainment',
                'tech', 'war', 'other']
 news_genres.reverse()
 
+by_country = newspapers_dataset.groupby(by='country').sum().copy()
+by_country = by_country.reset_index()
+by_country['total_news'] = total_news_by_country
+by_country['country_key'] = country_keys
+by_country['positivity_rate'] = by_country['positive'] / \
+    by_country['total_news']
+by_country['positivity_rate_per100'] = round(
+    (by_country['positive'] / by_country['total_news'])*100)
+by_country['negatives'] = by_country['total_news'] - by_country['positive']
+
 app = Dash(
     __name__,
     title='Newspaper Analyzer',
     external_stylesheets=[BS5, FONT],
+    external_scripts=[ICONS],
     meta_tags=[{"name": "viewport",
                 "content": "width=device-width, initial-scale=1"}],
     suppress_callback_exceptions=True
@@ -41,6 +67,8 @@ def dataframe_filter(date, country):
     """
     Function that filter data by country and date.
     """
+    if not date or not country:
+        raise PreventUpdate
     by_country = newspapers_dataset.query(
         f'country == "{country}" & scrape_date == "{date}"')
     return by_country.to_json(orient='split', date_format='iso')
@@ -57,49 +85,29 @@ def nav_bar():
     ], className='navbar navbar-light bg-light glass-navbar')
 
 
-def selector_card():
-    # Converting dates into Pandas Timestamp for create a list
-    # which contains days with no news in case it was necessary
-    margin_of_dates = pd.date_range(newspapers_dataset['scrape_date'].min(),
-                                    newspapers_dataset['scrape_date'].max()).tolist()
-    enable_days = [i for i in newspapers_dataset['scrape_date'].unique()]
-    disabled_days = [i for i in margin_of_dates if i not in enable_days]
-
-    return dbc.Card([
-        dbc.CardHeader(className='glass-card glass-card-header',
-                       children='Selector', style={'font-size': '18px'}),
-        dbc.CardBody([
-            html.P(children='Filter by:',
-                   style={'height': '15px'}
-                   ),
-            dcc.DatePickerSingle(
-                min_date_allowed=newspapers_dataset['scrape_date'].min(),
-                max_date_allowed=newspapers_dataset['scrape_date'].max(),
-                disabled_days=disabled_days,
-                calendar_orientation='vertical',
-                first_day_of_week=1,
-                date=newspapers_dataset['scrape_date'].max(),
-                id='date-picker',
-                className='selector',
-                style={'z-index': '40'}
-            ),
-            dcc.Dropdown(
-                options=by_country['country'].tolist(),
-                placeholder="Select a country",
-                value='Argentina',
-                className='selector country-dropdown',
-                id='country-dropdown',
-                style={'z-index': '30'}
-            ),
-        ], class_name='selector',
-            style={'z-index': '5'}
-        )
-    ], style={'background': 'rgba(255, 255, 255, 0)',
-              'border': 'none',
-              'padding-top': '10px',
-              'padding-bottom': '10px',
-              }
-    )
+def about():
+    return dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                html.Center([
+                    html.P(['Newspaper Analytics is a website where I show statistics \
+                    about newspapers around the world, like positivity and genres.'],
+                           style={'margin-top': '10px'}),
+                    html.P(
+                        ['If you feel curious, you can see the code on github! And check my LinkedIn Profile!']),
+                    html.A(className='fa-brands fa-square-github fa-beat',
+                           href='https://github.com/juanesc396',
+                           target='blank_'
+                           ),
+                    html.A(className='fa-brands fa-linkedin fa-beat',
+                           href='https://www.linkedin.com/in/juanesc396/',
+                           target='blank_'
+                           )
+                ], style={'margin-bottom': '10px'})
+            ], class_name='glass-card glass-footer'
+            )
+        ], width=12)
+    ], justify="center")
 
 
 @app.callback(
@@ -214,7 +222,6 @@ def posneg_world():
                                   hovertext=hover,
                                   marker_line_color='white',
                                   colorbar_title='Positivity Rate',
-                                  legendwidth=300
                                   ))
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
                       paper_bgcolor='rgba(0,0,0,0)',
@@ -229,18 +236,84 @@ def posneg_world():
     return fig
 
 
+def selector_card():
+    # Converting dates into Pandas Timestamp for create a list
+    # which contains days with no news in case it was necessary
+    margin_of_dates = pd.date_range(newspapers_dataset['scrape_date'].min(),
+                                    newspapers_dataset['scrape_date'].max()).tolist()
+    enable_days = [datetime.strptime(i, '%Y-%m-%d')
+                   for i in newspapers_dataset['scrape_date'].unique()]
+    disabled_days = [i for i in margin_of_dates if i not in enable_days]
+
+    return dbc.Card([
+        dbc.CardHeader([
+            dbc.Accordion([
+                    dbc.AccordionItem('You can filter the data by date(last 10 days) and country below:', 
+                                      title='Selector', class_name='transparent-bg')
+                ], flush=True, 
+                start_collapsed=False,
+                class_name='transparent-bg'
+                )
+        ], className='glass-card glass-card-header',
+        ),
+        dbc.CardBody([            
+            dbc.Row([
+                dbc.Col([
+                    dcc.DatePickerSingle(
+                        min_date_allowed=newspapers_dataset['scrape_date'].min(
+                        ),
+                        max_date_allowed=newspapers_dataset['scrape_date'].max(
+                        ),
+                        disabled_days=disabled_days,
+                        calendar_orientation='vertical',
+                        first_day_of_week=1,
+                        date=newspapers_dataset['scrape_date'].max(),
+                        id='date-picker',
+                        className='selector',
+                        style={'z-index': '40'}
+                    )
+                ], xs=6, sm=6, md=6, lg=6, xl=6),
+                dbc.Col([
+                    dcc.Dropdown(
+                        options=by_country['country'].tolist(),
+                        placeholder="Select a country",
+                        value='Argentina',
+                        className='selector country-dropdown',
+                        id='country-dropdown',
+                        style={'z-index': '30'}
+                    ),
+                ], xs=6, sm=6, md=6, lg=6, xl=6)
+            ])
+
+        ], class_name='selector',
+            style={'z-index': '5'}
+        )
+    ], style={'background': 'rgba(255, 255, 255, 0)',
+              'border': 'none',
+              'padding-top': '10px',
+              'padding-bottom': '10px',
+              }
+    )
+
+
 def world_card():
     return dbc.Row([
         dbc.Col([
             dbc.Card([
-                dbc.CardHeader(['The map of Positivity'],
-                               className='glass-card glass-card-header',
-                               style={'font-size': '18px'}),
+                dbc.CardHeader([
+                    dbc.Accordion([
+                        dbc.AccordionItem('Shows the ratio of positivity by country.', 
+                                            title='The map of Positivity', class_name='transparent-bg')
+                    ], flush=True, 
+                    start_collapsed=True,
+                    class_name='transparent-bg',
+                    )
+                ], className='glass-card glass-card-header',
+                   style={'font-size': '18px'}),
                 dbc.CardBody([dcc.Graph(figure=posneg_world(),
                                         responsive=True)
                 ], className='glass-card')
-            ], style={'background': 'rgba(255, 255, 255, 0)',
-                      'border': 'none'}
+            ], class_name='transparent-bg'
             )
         ], xs=12, sm=12, md=12, lg=6, xl=6
         )
@@ -252,14 +325,19 @@ def world_card():
 def bar_card():
     return dbc.Col([
         dbc.Card([
-            dbc.CardHeader(['News by Genre'],
-                           className='glass-card glass-card-header',
-                           style={'font-size': '18px'}
-                           ),
+            dbc.CardHeader([
+                dbc.Accordion([
+                    dbc.AccordionItem('This graph shows news by genre and it\'s positivity , \
+                                      filtered by the country and date selected.', 
+                                      title='News by Genre', class_name='transparent-bg')
+                ], flush=True, 
+                start_collapsed=True,
+                class_name='transparent-bg'
+                )
+            ], className='glass-card glass-card-header'),
             dbc.CardBody(dcc.Graph(id='cat_bar_chart'),
                          className='glass-card')
-        ], style={'background': 'rgba(255, 255, 255, 0)',
-                  'border': 'none'}
+        ], class_name='transparent-bg'
         )
     ], xs=12, sm=12, md=12, lg=4, xl=4,
         style={'padding-top': '10px'}
@@ -269,38 +347,48 @@ def bar_card():
 def pie_card():
     return dbc.Col([
         dbc.Card([
-            dbc.CardHeader(['Positivity Pie Chart'],
-                           className='glass-card glass-card-header',
-                           style={'font-size': '18px'}),
+            dbc.CardHeader([
+                dbc.Accordion([
+                    dbc.AccordionItem('This graph shows how divides news in positive \
+                                      and negative by country and date selected.', 
+                                      title='Positivity Pie Chart', class_name='transparent-bg')
+                ], flush=True, 
+                start_collapsed=True,
+                class_name='transparent-bg'
+                )
+            ], className='glass-card glass-card-header'
+            ),
             dbc.CardBody([dcc.Graph(id='pie-chart')],
                          className='glass-card')
-        ], style={'background': 'rgba(255, 255, 255, 0)',
-                  'border': 'none'}
+        ], class_name='transparent-bg'
         )
     ], xs=12, sm=12, md=12, lg=4, xl=4,
-        style={'padding-top': '10px'}
+        style={'padding-top': '10px', 'height': '100%'}
     )
 
 
-def table_news():
+def table_news_card():
     return dbc.Card([
-        dbc.CardHeader(['News Selector'],
-                       className='glass-card glass-card-header',
-                       style={'font-size': '18px'}
-                       ),
+        dbc.CardHeader([
+            dbc.Accordion([
+                dbc.AccordionItem('Allows select a random news to see how it classificate.', 
+                                      title='Random news selector', class_name='transparent-bg')
+            ], flush=True, 
+            start_collapsed=True,
+            class_name='transparent-bg',
+            )
+        ], className='glass-card glass-card-header',
+        ),
         dbc.CardBody([
             html.Div(id='news-table'),
-            html.Center(html.Button('Select a random News',
-                                    id='random-button',
-                                    className='btn btn-secondary button-random-news'
-                                    ), style={'margin-top': '8px',
-                                              'font-size': '15px'}
+            html.Button('Select a random News',
+                        id='random-button',
+                        className='btn btn-secondary button-random-news'
             )
         ], className='glass-card',
-            style={'height': '281px'}
+           style={'height': '306px'}
         )
-    ], style={'background': 'rgba(255, 255, 255, 0)',
-              'border': 'none'}
+    ], class_name='transparent-bg'    
     )
 
 
@@ -311,17 +399,18 @@ app.layout = html.Div([
         dbc.Row([
                 dbc.Col([
                     selector_card(),
-                    table_news()
+                    table_news_card()
                 ]),
                 bar_card(),
                 pie_card()
                 ], style={'margin-bottom': '10px'}
                 ),
-        world_card()
+        world_card(),
+        about()
     ], fluid=True
     )
 ], className='glass-background'
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run()
